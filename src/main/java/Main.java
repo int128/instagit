556e719bd880543fa03de71f733ed9f5c622380d
int128 <int128@gmail.com>
2015-01-06 17:27:33 +0900

    Add Jetty and JGit servlet app

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,19 @@
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jgit.http.server.GitServlet;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-        for (String arg : args) {
-            System.out.println(arg);
-        }
+    public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
+
+        ServletHandler handler = new ServletHandler();
+        ServletHolder holder = handler.addServletWithMapping(GitServlet.class, "/*");
+        holder.setInitParameter("base-path", "..");
+        holder.setInitParameter("export-all", "1");
+
+        server.setHandler(handler);
+        server.start();
+        server.join();
     }
 }
