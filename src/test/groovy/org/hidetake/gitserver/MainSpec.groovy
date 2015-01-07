6355fc01f7b8843d734d0838e14e89c847373583
int128 <int128@gmail.com>
2015-01-08 00:42:29 +0900

    Add spec of usage

--- src/test/groovy/org/hidetake/gitserver/MainSpec.groovy
+++ src/test/groovy/org/hidetake/gitserver/MainSpec.groovy
@@ -1,14 +1,20 @@
 package org.hidetake.gitserver
 
 import org.eclipse.jetty.server.ServerConnector
+import org.junit.Rule
+import org.junit.contrib.java.lang.system.ExpectedSystemExit
+import org.junit.contrib.java.lang.system.internal.CheckExitCalled
 import spock.lang.Specification
 
 class MainSpec extends Specification {
 
+    @Rule
+    final ExpectedSystemExit exit = ExpectedSystemExit.none()
+
     def "server should listen on localhost:8080 at default"() {
         given:
         def options = MainOptions.parse()
-        def main = new Main(options);
+        def main = new Main(options)
 
         when:
         main.server.start()
@@ -21,6 +27,29 @@ class MainSpec extends Specification {
 
         cleanup:
         main.server.stop()
+    }
+
+    def "unknown option should show usage"() {
+        given:
+        def stderr = System.err
+        def stderrBuffer = new ByteArrayOutputStream()
+        System.err = new PrintStream(stderrBuffer)
+
+        exit.expectSystemExit()
+
+        when:
+        Main.main '-h'
+
+        then:
+        CheckExitCalled e = thrown()
+        e.status == 1
+
+        and:
+        stderrBuffer.toString('UTF-8').contains('usage:')
+
+        cleanup:
+        System.err = stderr
+        System.err.println(stderrBuffer.toString('UTF-8'))
     }
 
 }
